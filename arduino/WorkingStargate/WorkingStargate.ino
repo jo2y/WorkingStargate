b2d2dc29d319a780c2f14933a4f9b19ccc72eeed
James O'Kane <jo2y@midnightlinux.com>
2016-09-14 23:40:12 -0700

    Slow down the chevron locking.
    git-svn-id: file:///home/jo2y/svn/WorkingStargate/MK2@650 90519734-482e-411e-a2a4-e53c5c4f76ba

--- arduino/WorkingStargate/WorkingStargate.ino
+++ arduino/WorkingStargate/WorkingStargate.ino
@@ -63,7 +63,7 @@
 #define CHEVRON_MOTOR_PORT 2
 // How fast should the motors be moved? The values here are likely suitable.
 #define GLYPH_MOTOR_SPEED 50  // RPMs
-#define CHEVRON_MOTOR_SPEED 50  // RPMs
+#define CHEVRON_MOTOR_SPEED 10  // RPMs
 
 //
 // Audio Settings
